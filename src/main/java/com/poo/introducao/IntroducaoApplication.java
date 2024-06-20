e51033d8891ae755d609559651d47d1ad69816ec
wagner gomes <wagnergomes2132@gmail.com>
2024-06-20 18:10:15 -0300

    Lista03 resolvidos

--- src/main/java/com/poo/introducao/IntroducaoApplication.java
+++ src/main/java/com/poo/introducao/IntroducaoApplication.java
@@ -9,19 +9,31 @@ public class IntroducaoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(IntroducaoApplication.class, args);
 
-		Lista02 lista2 = new Lista02();
-		lista2.exec8();
+
+		Lista03 lista3 = new Lista03();
+		lista3.exerci7();
+
+		//Lista02 lista2 = new Lista02();
+		// lista2.exec1();
+		//lista2.exec2();
+		//lista2.exec3();
+		//lista2.exec4();
+		//lista2.exec5();
+		//lista2.exec6();
+		//lista2.exec7();
+		//lista2.exec8();
 
 
-		Lista01 lista = new Lista01();
-		lista.ex1();
-		lista.ex2();
-		lista.ex3();
-		lista.ex4();
-		lista.ex5();
-		lista.ex6();
-		lista.ex7();
-		lista.ex8();
+
+		//Lista01 lista = new Lista01();
+		//lista.ex1();
+		//lista.ex2();
+		//lista.ex3();
+		//lista.ex4();
+		//lista.ex5();
+		//lista.ex6();
+		//lista.ex7();
+		//lista.ex8();
 
 
 	}
---
+++ src/main/java/com/poo/introducao/Lista03.java
@@ -0,0 +1,220 @@
+package com.poo.introducao;
+
+import java.util.Scanner;
+
+import javax.sound.sampled.SourceDataLine;
+
+public class Lista03 {
+    public void exerci1() {
+        Scanner scanner = new Scanner(System.in);
+        int segundos = 0;
+
+        System.out.println("Digite um numero entre 1 e 59:");
+        while (true)
+            try {
+                segundos = Integer.parseInt(scanner.nextLine());
+                if (segundos >= 1 && segundos <= 59) {
+                    break;
+                } else {
+                    System.out.println("Número inválido. Digite um número entre 1 e 59:");
+
+                }
+
+            } catch (NumberFormatException e) {
+                System.out.println("Entrada inválida. Digite um número valido:");
+
+            }
+        System.out.println("Contagem iniciada para " + segundos + "segundos...");
+        try {
+            for (int i = segundos; i > 0; i--) {
+                System.out.println("Faltam" + i + " segundos...");
+                Thread.sleep(1000);
+
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Tempo esgotado!");
+
+    }
+
+    void exerci2() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número para ver a tabuada: ");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Erro: Digite um número inteiro válido.");
+            scanner.next();
+            System.out.print("Digite um número para ver a tabuada: ");
+        }
+
+        int numero = scanner.nextInt();
+
+        System.out.println("Tabuada do " + numero + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+    }
+
+    void exerci3() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número inteiro: ");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Erro: Digite um número inteiro válido.");
+            scanner.next();
+            System.out.print("Digite o primeiro número inteiro: ");
+        }
+        int primeiroNumero = scanner.nextInt();
+
+        System.out.print("Digite o segundo número inteiro: ");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Erro: Digite um número inteiro válido.");
+            scanner.next();
+            System.out.print("Digite o segundo número inteiro: ");
+        }
+        int segundoNumero = scanner.nextInt();
+
+        int inicio = Math.min(primeiroNumero, segundoNumero);
+        int fim = Math.max(primeiroNumero, segundoNumero);
+
+        System.out.println("Números pares entre " + inicio + " e " + fim + ":");
+        for (int i = inicio; i <= fim; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    void exerci4() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Pessoa 1, escolha um número (entre 1 e 10): ");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Erro: Digite um número inteiro válido.");
+            scanner.next();
+            System.out.print("Pessoa 1, escolha um número (entre 1 e 10): ");
+        }
+        int numeroEscolhido = scanner.nextInt();
+
+        for (int i = 0; i < 50; i++) {
+            System.out.println();
+        }
+
+        System.out.println("Pessoa 2, tente adivinhar o número escolhido.");
+        int tentativa;
+        while (true) {
+            System.out.print("Digite sua tentativa: ");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Erro: Digite um número inteiro válido.");
+                scanner.next();
+                System.out.print("Digite sua tentativa: ");
+            }
+            tentativa = scanner.nextInt();
+
+            if (tentativa == numeroEscolhido) {
+                System.out.println("Parabéns! Você adivinhou o número.");
+                break;
+            } else if (Math.abs(tentativa - numeroEscolhido) <= 2) {
+                System.out.println("Quase lá!");
+            } else if (tentativa < numeroEscolhido) {
+                System.out.println("Muito baixo.");
+            } else {
+                System.out.println("Muito alto.");
+            }
+        }
+    }
+
+    void exerci5() {
+        Scanner scanner = new Scanner(System.in);
+
+        String loginCorreto = "wagner";
+        String senhaCorreta = "w1234567";
+
+        int tentativasRestantes = 3;
+
+        while (tentativasRestantes > 0) {
+            System.out.print("Digite seu login: ");
+            String login = scanner.nextLine();
+
+            System.out.print("Digite sua senha: ");
+            String senha = scanner.nextLine();
+
+            if (login.equals(loginCorreto) && senha.equals(senhaCorreta)) {
+                System.out.println("Acesso concedido!");
+                break;
+            } else {
+                tentativasRestantes--;
+                if (tentativasRestantes == 0) {
+                    System.out.println("Acesso bloqueado. Número de tentativas excedido.");
+                } else {
+                    if (!login.equals(loginCorreto)) {
+                        System.out.println("Login incorreto.");
+                    }
+                    if (!senha.equals(senhaCorreta)) {
+                        System.out.println("Senha incorreta.");
+                    }
+                    if (tentativasRestantes == 1) {
+                        System.out.println("Última tentativa, mais um erro seu acesso será bloqueado!");
+                    } else {
+                        System.out.println("Você tem " + tentativasRestantes + " tentativas restantes.");
+                    }
+                }
+            }
+        }
+    }
+
+    void exerci6(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o número de andares da pirâmide: ");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Erro: Digite um número inteiro válido.");
+            scanner.next(); 
+            System.out.print("Digite o número de andares da pirâmide: ");
+        }
+        int numeroAndares = scanner.nextInt();
+
+        
+        for (int i = 1; i <= numeroAndares; i++) {
+            
+            for (int j = 1; j <= numeroAndares - i; j++) {
+                System.out.print(" ");
+            }
+            
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println(); 
+        }
+    }
+
+    void exerci7(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a largura do retângulo (em asteriscos): ");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Erro: Digite um número inteiro válido.");
+            scanner.next(); 
+            System.out.print("Digite a largura do retângulo (em asteriscos): ");
+        }
+        int largura = scanner.nextInt();
+
+        System.out.print("Digite a altura do retângulo (em asteriscos): ");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Erro: Digite um número inteiro válido.");
+            scanner.next(); 
+            System.out.print("Digite a altura do retângulo (em asteriscos): ");
+        }
+        int altura = scanner.nextInt();
+
+       
+        for (int i = 0; i < altura; i++) {
+            for (int j = 0; j < largura; j++) {
+                System.out.print("*");
+            }
+            System.out.println(); 
+        }
+
+    }
+
+}
